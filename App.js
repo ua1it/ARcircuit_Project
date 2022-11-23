550b4a561ad36c5923197d31c41d4b5cdd4acfc1
ua1it <hoo9909@naver.com>
2022-11-23 23:38:54 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,114 @@
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, Button, Animated, PanResponder } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//Screens
+import ARScreen from './Screens/ARScreen';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.screen}>
+      <Text>HomeScreen</Text>
+      <Button
+        title="Go to Details"
+        onPress={ () => navigation.navigate('Details')}
+      />
+    </View>
+  )
+}
+
+const DetailsScreen = ({navigation}) => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value
+        });
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          { dx: pan.x, dy: pan.y }
+        ],
+        {useNativeDriver: false}
+      ),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      }
+    })
+  ).current;
+
+  return (
+    <View style={styles.screen}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details again"
+        onPress={ () => navigation.push('Details')}
+      />
+      <Button 
+        title="Go to Home"
+        onPress={ () => navigation.navigate('Home')}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => navigation.goBack()}
+      />
+      <Button 
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+      <Button
+        title="Go AR"
+        onPress={() => navigation.navigate('ARScreen')}
+      />
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }]
+        }}
+        {...panResponder.panHandlers}
+      >
+        <View style = {styles.box} />
+      </Animated.View>
+    </View>
+  )
+}
+// 앱이 각 화면이 전환될 수 있는 기본 틀을 제공한다.
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    //네비게이션의 트리를 관리해주는 컴포넌트
+    <NavigationContainer>
+      {/* 네비게이션 기본틀의 스택을 생성 */}
+      <Stack.Navigator
+        detachInactiveScreens={false}
+      >
+        {/* 해당스택에 들어갈 화면 요소를 넣어준다. */}
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Details" component={DetailsScreen}/>
+        <Stack.Screen name="ARScreen" component={ARScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: "blue",
+    borderRadius: 5
+  }
+})
+
+export default App;
---
+++ Screens/ARScreen.js
@@ -0,0 +1,113 @@
+import React, { useState, useRef } from 'react';
+import { StyleSheet, SafeAreaView, Button, PermissionsAndroid, Platform, View } from 'react-native';
+import {
+  ViroARScene,
+  ViroText,
+  ViroTrackingStateConstants,
+  ViroARSceneNavigator,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroQuad,
+  ViroAnimations,
+  ViroCamera,
+  ViroFlexView
+} from '@viro-community/react-viro';
+
+
+
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState('Initializing AR...');
+
+  function onInitialized(state, reason) {
+    console.log('guncelleme', state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText('AR Demo!');
+    } else if (state === ViroTrackingStateConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+
+  return (
+    <View>
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+      <ViroAmbientLight color={"#aaaaaa"} />
+      <ViroARPlaneSelector>
+        <ViroNode position={[0, -2, 0]} dragType="FixedToWorld" onDrag={() => { }} >
+
+          {/* Spotlight to cast light on the object and a shadow on the surface, see
+              the Viro documentation for more info on lights & shadows */}
+          <ViroSpotLight
+            innerAngle={5}
+            outerAngle={45}
+            direction={[0, -1, -.2]}
+            position={[0, 3, 0]}
+            color="#ffffff"
+            castsShadow={true}
+            influenceBitMask={2}
+            shadowMapSize={2048}
+            shadowNearZ={2}
+            shadowFarZ={5}
+            shadowOpacity={.7} />
+          <Viro3DObject
+            source={require('../models/emoji_smile.vrx')}
+            //source={require('../models/kielich.obj')}
+            position={[0, 0, 0]}
+            scale={[.2, .2, .2]}
+            type="VRX"
+            lightReceivingBitMask={3}
+            shadowCastingBitMask={2}
+            transformBehaviors={['billboardY']}
+            resources={[require('../models/emoji_smile_diffuse.png'),
+            require('../models/emoji_smile_specular.png'),
+            require('../models/emoji_smile_normal.png')]} />
+          <ViroQuad
+            rotation={[-90, 0, 0]}
+            width={.5} height={.5}
+            arShadowReceiver={true}
+            lightReceivingBitMask={2} />
+        </ViroNode>
+      </ViroARPlaneSelector>
+      {/* <ViroNode position={[0, 0, -1]} dragType="FixedToWorld" onDrag={() => { }} >
+        <Viro3DObject
+          source={require('./emoji_smile.vrx')}
+          position={[0, .1, 0]}
+          scale={[.2, .2, .2]}
+          type="VRX"
+        />
+      </ViroNode> */}
+    </ViroARScene>
+    </View>
+  );
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
