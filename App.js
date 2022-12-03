5961598287aa58a1e1db949915dd662e0e7969ca
ua1it <hoo9909@naver.com>
2022-12-03 23:11:33 +0900

    HomeScreen Modal

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
-import React, { useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity, Animated, StatusBar, PanResponder, ImageBackground } from 'react-native';
+import React, { useRef, useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button, TouchableOpacity, Animated, StatusBar, PanResponder, ImageBackground, Modal, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from 'react-native-splash-screen';
+
+//Storage for other Screen
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 //Screens
 import ARScreen from './Screens/ARScreen';
 
 const HomeScreen = ({navigation}) => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.screen}>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+      >
+        <Pressable style = {styles.centeredView} onPress = {() => setModalVisible(false)}>
+          <View style = {styles.modalView} >
+            <Text style = {{fontWeight: 'bold'}}>Choose Your State</Text>
+            <View style = {{flexDirection: 'row', justifyContent:'space-between'}}>
+              <TouchableOpacity style = {styles.modalButton} onPress = {()=>{
+                setModalVisible(!modalVisible);
+                navigation.navigate('ARScreen');
+              }}><Text>Detect</Text></TouchableOpacity>
+              <Text>{'   '}</Text>
+              <TouchableOpacity style = {styles.modalButton} onPress = {()=>{
+                setModalVisible(!modalVisible);
+                navigation.navigate('ARScreen');
+              }}><Text>AR</Text></TouchableOpacity>
+            </View>
+          </View>
+        </Pressable>
+      </Modal>
       <ImageBackground source={require("./images/homeImage.jpg")} resizeMode="cover" style={styles.image}>
         <View style = {styles.titleView}>
           <Text style = {styles.title}>
@@ -19,7 +45,7 @@ const HomeScreen = ({navigation}) => {
             Circuit{"\n"}
           </Text>
         </View>
-        <TouchableOpacity style = {styles.goButton} onPress = {() => navigation.navigate('Details')}>
+        <TouchableOpacity style = {styles.goButton} onPress = {() => setModalVisible(true)}>
           <Text style = {styles.go}>GO</Text>
         </TouchableOpacity>
         <View style = {styles.explainView}>
@@ -38,65 +64,66 @@ const HomeScreen = ({navigation}) => {
   )
 }
 
-const DetailsScreen = ({navigation}) => {
-  const pan = useRef(new Animated.ValueXY()).current;
+// const DetailsScreen = ({navigation}) => {
+//   const pan = useRef(new Animated.ValueXY()).current;
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => {
-        pan.setOffset({
-          x: pan.x._value,
-          y: pan.y._value
-        });
-      },
-      onPanResponderMove: Animated.event(
-        [
-          null,
-          { dx: pan.x, dy: pan.y }
-        ],
-        {useNativeDriver: false}
-      ),
-      onPanResponderRelease: () => {
-        pan.flattenOffset();
-      }
-    })
-  ).current;
+//   const panResponder = useRef(
+//     PanResponder.create({
+//       onMoveShouldSetPanResponder: () => true,
+//       onPanResponderGrant: () => {
+//         pan.setOffset({
+//           x: pan.x._value,
+//           y: pan.y._value
+//         });
+//       },
+//       onPanResponderMove: Animated.event(
+//         [
+//           null,
+//           { dx: pan.x, dy: pan.y }
+//         ],
+//         {useNativeDriver: false}
+//       ),
+//       onPanResponderRelease: () => {
+//         pan.flattenOffset();
+//       }
+//     })
+//   ).current;
 
-  return (
-    <View style={styles.screen}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details again"
-        onPress={ () => navigation.push('Details')}
-      />
-      <Button 
-        title="Go to Home"
-        onPress={ () => navigation.navigate('Home')}
-      />
-      <Button
-        title="Go Back"
-        onPress={() => navigation.goBack()}
-      />
-      <Button 
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
-      <Button
-        title="Go AR"
-        onPress={() => navigation.navigate('ARScreen')}
-      />
-      <Animated.View
-        style={{
-          transform: [{ translateX: pan.x }, { translateY: pan.y }]
-        }}
-        {...panResponder.panHandlers}
-      >
-        <View style = {styles.box} />
-      </Animated.View>
-    </View>
-  )
-}
+//   return (
+//     <View style={styles.screen}>
+//       <Text>Details Screen</Text>
+//       <Button
+//         title="Go to Details again"
+//         onPress={ () => navigation.push('Details')}
+//       />
+//       <Button 
+//         title="Go to Home"
+//         onPress={ () => navigation.navigate('Home')}
+//       />
+//       <Button
+//         title="Go Back"
+//         onPress={() => navigation.goBack()}
+//       />
+//       <Button 
+//         title="Go back to first screen in stack"
+//         onPress={() => navigation.popToTop()}
+//       />
+//       <Button
+//         title="Go AR"
+//         onPress={() => navigation.navigate('ARScreen')}
+//       />
+//       <Animated.View
+//         style={{
+//           transform: [{ translateX: pan.x }, { translateY: pan.y }]
+//         }}
+//         {...panResponder.panHandlers}
+//       >
+//         <View style = {styles.box} />
+//       </Animated.View>
+//     </View>
+//   )
+// }
+
 // 앱이 각 화면이 전환될 수 있는 기본 틀을 제공한다.
 const Stack = createStackNavigator();
 
@@ -121,7 +148,7 @@ const App = () => {
       >
         {/* 해당스택에 들어갈 화면 요소를 넣어준다. */}
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Details" component={DetailsScreen}/>
+        {/* <Stack.Screen name="Details" component={DetailsScreen}/> */}
         <Stack.Screen name="ARScreen" component={ARScreen} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
@@ -180,6 +207,38 @@ const styles = StyleSheet.create({
   explainContent: {
     color : 'black',
     fontSize: 13,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    position: 'absolute',
+    borderRadius: 20,
+    padding: 25,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalButton: {
+    marginTop: '10%',
+    marginBottom: '-5%',
+    backgroundColor: 'white',
+    padding: 12,
+    borderRadius: 10,
+    elevation: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
--- Screens/ARScreen.js
+++ Screens/ARScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useRef } from 'react';
-import { StyleSheet, SafeAreaView, Button, PermissionsAndroid, Platform, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, PermissionsAndroid, Platform, View, TouchableOpacity } from 'react-native';
 import {
   ViroARScene,
   ViroText,
+  ViroButton,
   ViroTrackingStateConstants,
   ViroARSceneNavigator,
   Viro3DObject,
@@ -32,64 +33,64 @@ const HelloWorldSceneAR = () => {
 
   return (
     <View>
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-      <ViroAmbientLight color={"#aaaaaa"} />
-      <ViroARPlaneSelector>
-        <ViroNode position={[0, -2, 0]} dragType="FixedToWorld" onDrag={() => { }} >
-
-          {/* Spotlight to cast light on the object and a shadow on the surface, see
-              the Viro documentation for more info on lights & shadows */}
-          <ViroSpotLight
-            innerAngle={5}
-            outerAngle={45}
-            direction={[0, -1, -.2]}
-            position={[0, 3, 0]}
-            color="#ffffff"
-            castsShadow={true}
-            influenceBitMask={2}
-            shadowMapSize={2048}
-            shadowNearZ={2}
-            shadowFarZ={5}
-            shadowOpacity={.7} />
-          <Viro3DObject
-            //source={require('../models/emoji_smile.vrx')}
-            //source={require('../models/kielich.obj')}
-            source={require('../models/tinker.obj')}
-            position={[0, 0, 0]}
-            scale={[.01, .01, .01]}
-            type="OBJ"
-            lightReceivingBitMask={3}
-            shadowCastingBitMask={2}
-            transformBehaviors={['billboardY']}
-            resources={[
-            // require('../models/emoji_smile_diffuse.png'),
-            // require('../models/emoji_smile_specular.png'),
-            // require('../models/emoji_smile_normal.png')
-            require('../models/obj.mtl')
-          ]} 
-            />
-          <ViroQuad
-            rotation={[-90, 0, 0]}
-            width={.5} height={.5}
-            arShadowReceiver={true}
-            lightReceivingBitMask={2} />
-        </ViroNode>
-      </ViroARPlaneSelector>
-      {/* <ViroNode position={[0, 0, -1]} dragType="FixedToWorld" onDrag={() => { }} >
-        <Viro3DObject
-          source={require('./emoji_smile.vrx')}
-          position={[0, .1, 0]}
-          scale={[.2, .2, .2]}
-          type="VRX"
+      <ViroARScene onTrackingUpdated={onInitialized}>
+        <ViroText
+          text={text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
         />
-      </ViroNode> */}
-    </ViroARScene>
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroARPlaneSelector>
+          <ViroNode position={[0, -2, 0]} dragType="FixedToWorld" onDrag={() => { }} >
+
+            {/* Spotlight to cast light on the object and a shadow on the surface, see
+                the Viro documentation for more info on lights & shadows */}
+            <ViroSpotLight
+              innerAngle={5}
+              outerAngle={45}
+              direction={[0, -1, -.2]}
+              position={[0, 3, 0]}
+              color="#ffffff"
+              castsShadow={true}
+              influenceBitMask={2}
+              shadowMapSize={2048}
+              shadowNearZ={2}
+              shadowFarZ={5}
+              shadowOpacity={.7} />
+            <Viro3DObject
+              //source={require('../models/emoji_smile.vrx')}
+              //source={require('../models/kielich.obj')}
+              source={require('../models/tinker.obj')}
+              position={[0, 0, 0]}
+              scale={[.01, .01, .01]}
+              type="OBJ"
+              lightReceivingBitMask={3}
+              shadowCastingBitMask={2}
+              transformBehaviors={['billboardY']}
+              resources={[
+              // require('../models/emoji_smile_diffuse.png'),
+              // require('../models/emoji_smile_specular.png'),
+              // require('../models/emoji_smile_normal.png')
+              require('../models/obj.mtl')
+            ]} 
+              />
+            <ViroQuad
+              rotation={[-90, 0, 0]}
+              width={.5} height={.5}
+              arShadowReceiver={true}
+              lightReceivingBitMask={2} />
+          </ViroNode>
+        </ViroARPlaneSelector>
+        {/* <ViroNode position={[0, 0, -1]} dragType="FixedToWorld" onDrag={() => { }} >
+          <Viro3DObject
+            source={require('./emoji_smile.vrx')}
+            position={[0, .1, 0]}
+            scale={[.2, .2, .2]}
+            type="VRX"
+          />
+        </ViroNode> */}
+      </ViroARScene>
     </View>
   );
 };
