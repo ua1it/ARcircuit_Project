836ef5b08f87349ea67fa45d7112d63e1e04ea2d
ua1it <hoo9909@naver.com>
2022-12-09 01:12:23 +0900

    modelModify, board hand area

--- Screens/ARScreen.js
+++ Screens/ARScreen.js
@@ -32,15 +32,16 @@ import {
 } from '@viro-community/react-viro';
 
 
-function ARScreen(props){
+let detectOrBase = 'base';
 
+function ARScreen(){
   const [hasARInitialized, setHasARInitialized] = useState(false);
   const [text,setText] = useState('Initializing AR...');
 
   useEffect(()=>{
-    if (!hasARInitialized === ViroTrackingStateConstants.TRACKING_NORMAL) {
+    if (!hasARInitialized) {
       setHasARInitialized(true);
-      setText("This place is IEEC AR!");
+      setText("IEEC AR!");
     }
   },[hasARInitialized])
 
@@ -54,53 +55,59 @@ function ARScreen(props){
 						style={styles.helloWorldTextStyle}
 						/>
 					<ViroAmbientLight color={"#aaaaaa"} influenceBitMask={1} />
-
-          <ViroNode position={[0, -2, 0]} dragType="FixedToWorld" onDrag={() => { }} >
+          {
+            detectOrBase === 'base'?(
+              <ViroNode position={[0, -2, 0]} dragType="FixedDistance" onDrag={() => { }} >
             {/* Spotlight to cast light on the object and a shadow on the surface, see
             the Viro documentation for more info on lights & shadows */}
-            
-            <ViroSpotLight
-              innerAngle={5}
-              outerAngle={45}
-              direction={[0, -1, -.2]}
-              position={[0, 3, 0]}
-              color="#ffffff"
-              castsShadow={true}
-              influenceBitMask={2}
-              shadowMapSize={2048}
-              shadowNearZ={2}
-              shadowFarZ={5}
-              shadowOpacity={.7}
-              />
-            <Viro3DObject
-              source={require('./Arduino/ArduinoUno/arduino_uno.glb')}
-              position={[0, 0, 0]}
-              scale={[.1, .1, .1]}
-              type="GLB"
-              lightReceivingBitMask={3}
-              shadowCastingBitMask={2}
-              transformBehaviors={['billboardY']}
-              resources={[
-                require('./Arduino/ArduinoUno/textures/1775.png'),
-                require('./Arduino/ArduinoUno/textures/1785.png'),
-                require('./Arduino/ArduinoUno/textures/1795.png'),
-                require('./Arduino/ArduinoUno/textures/1805.png'),
-                require('./Arduino/ArduinoUno/textures/1815.png'),
-                require('./Arduino/ArduinoUno/textures/1825.png'),
-                require('./Arduino/ArduinoUno/textures/1835.png'),
-                require('./Arduino/ArduinoUno/textures/1844.png'),
-                require('./Arduino/ArduinoUno/textures/1854.png'),
-                require('./Arduino/ArduinoUno/textures/1919.png'),
-                require('./Arduino/ArduinoUno/textures/1929.png')
-              ]}
-              />
-            <ViroQuad
-              rotation={[-90, 0, 0]}
-              width={.5} height={.5}
-              arShadowReceiver={true}
-              lightReceivingBitMask={2}
-              />
-          </ViroNode>
+                <ViroSpotLight
+                  innerAngle={5}
+                  outerAngle={45}
+                  direction={[0, -1, -.2]}
+                  position={[0, 7, 0]}
+                  color="#ffffff"
+                  castsShadow={true}
+                  influenceBitMask={2}
+                  shadowMapSize={2048}
+                  shadowNearZ={2}
+                  shadowFarZ={5}
+                  shadowOpacity={.7}
+                  />
+                <Viro3DObject
+                  source={require('./Arduino/ArduinoUno/arduino_uno.glb')}
+                  position={[0, 0, 0]}
+                  scale={[0.5, 0.5, 0.5]}
+                  type="GLB"
+                  lightReceivingBitMask={3}
+                  shadowCastingBitMask={2}
+                  
+                  resources={[
+                    require('./Arduino/ArduinoUno/textures/1775.png'),
+                    require('./Arduino/ArduinoUno/textures/1785.png'),
+                    require('./Arduino/ArduinoUno/textures/1795.png'),
+                    require('./Arduino/ArduinoUno/textures/1805.png'),
+                    require('./Arduino/ArduinoUno/textures/1815.png'),
+                    require('./Arduino/ArduinoUno/textures/1825.png'),
+                    require('./Arduino/ArduinoUno/textures/1835.png'),
+                    require('./Arduino/ArduinoUno/textures/1844.png'),
+                    require('./Arduino/ArduinoUno/textures/1854.png'),
+                    require('./Arduino/ArduinoUno/textures/1919.png'),
+                    require('./Arduino/ArduinoUno/textures/1929.png')
+                  ]}
+                  />
+                <ViroQuad
+                  rotation={[-90, 0, 0]}
+                  width={.5} height={.5}
+                  arShadowReceiver={true}
+                  lightReceivingBitMask={2}
+                  />
+              </ViroNode>
+            ):(
+              <ViroNode>
+
+              </ViroNode>
+            )
+          }
 				</ViroARScene>
 			</View>
 		);
@@ -108,23 +115,24 @@ function ARScreen(props){
 
 export default ({route}: Navigation) => {
   console.log("state: "+route.params.state);
+  detectOrBase = route.params.state;
 	return (
 		<View style={{flex: 1}}>
       <ViroARSceneNavigator
         autofocus={true}
         initialScene={{scene: ARScreen,}}
       />
-      <View style={styles.boardFrame}>
-        {
-          route.params.state === 'detect'?(
+      {
+        route.params.state === 'detect'?(
+          <View style={styles.boardFrame}>
             <Text style = {styles.boardTitleText}>Place your board in this area!</Text>
-          ):(
-            <Text style = {styles.boardTitleText}>AR board works here!</Text>
-          )
-        }
-
-      </View>
+          </View>
+        ):(
+          <View></View>
+        )
+      }
       <View style={styles.handFrame}>
+      <Text style = {styles.handTitleText}>HandControl here!</Text>
 
       </View>
       {/* <View style={styles.abs}>
@@ -170,6 +178,10 @@ var styles = StyleSheet.create({
   boardTitleText: {
     marginLeft: '1%',
     color: 'white'
+  },
+  handTitleText: {
+    marginLeft: '3%',
+    color: 'white',
   },
 	helloWorldTextStyle: {
 		fontFamily: 'Arial',
